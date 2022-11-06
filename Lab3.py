8a2d1c9c9bc614a79564f7806efdcdc3070503c9
JGarbacz99 <jakub.garbacz99@gmail.com>
2022-11-06 12:50:53 +0100

    Laboratorium3

--- Lab3.py
+++ Lab3.py
@@ -1,1 +1,27 @@
-print('Hello world')
+print ("""
+1) dodawanie
+2) odejmowanie
+3) mnożenie
+4) dzielenie
+5) potęgowanie """)
+z=int(input('Jaką operację chcesz wykonać? '))
+if z>=6 or z<=0:
+    print("Niepoprawna operacja")
+    exit()
+x=float(input('Podaj x : '))
+y=float(input('Podaj y : '))
+if z==1:
+    w=x+y
+elif z==2:
+    w=x-y
+elif z==3:
+    w=x*y
+elif z==4:
+    if y==0:
+        print('Niepoprawne dane. Nie dzielimy przez 0!')
+        exit()
+    else:
+        w=x/y
+elif z==5:
+    w=x**y
+print(f'Wynik = {w}')
